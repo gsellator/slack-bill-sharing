f7ffc0a438802ecb2886fd3cb9eae8f05695276c
gsellator <adrien@gsell.me>
2016-07-04 16:43:17 +0200

    new bdd

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,3 @@
 export default {
-  bddUri: 'candidate.62.mongolayer.com:10098,candidate.61.mongolayer.com:10413/compta-prod?replicaSet=set-568151a379f11e24e7000a65'
+  bddUri: 'lamppost.0.mongolayer.com:10421,lamppost.9.mongolayer.com:10299/compta-prod?replicaSet=set-577a7350f648c7e5f1000a63'
 }
